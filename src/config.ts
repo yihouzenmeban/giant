7ba7016f474f9afd3cc668795e2d4c5728447eb9
yihouzenmeban <yihouzenmeban@gmail.com>
2023-01-09 15:42:12 +0800

    fix: 改下型号

--- src/config.ts
+++ src/config.ts
@@ -1,21 +1,23 @@
 // 用户 id
-export const userId = 'userId';
+export const userId = 'MjIyODUwOTVBRkJGMjRENzgzQkREQ0M2OTk4MDREMEE3MDI0MUM5OEE2NkJCOEQwRkExMkFCMTc5QjM5ODM2Qjg1RTc2QjMzQjE0QTFBNDVDNzQ0MjU5RDVCRjYwNkQzQjI2OThFQ0Y5NkJENzI3RjAyODQ3OEJGQkEwM0NGNTVBNDEzMDI5Njk2QzQ3RDlDQkQ4QUJCRDY4REE0NjU5QgGIANTGIANT';
 
 // 筛选地区
 export const province = 110000;
 
 // sku 列表
 export const skuMap = {
-  2250008123: 'slr 蓝',
-  2250008223: 'slr 黑',
-  2250103123: 'scr2 绿',
-  2250103223: 'scr2 红',
-  2250104123: 'scrd 银色',
-  2250104223: 'scrd 红色',
-  2150203323: 'speeder d1 亮白',
-  2150203223: 'speeder d1 灰色',
-  2150203123: 'speeder d1 深邃蓝',
+  // 2250008123: 'slr 蓝',
+  // 2250008223: 'slr 黑',
+  // 2250103123: 'scr2 绿',
+  // 2250103223: 'scr2 红',
+  // 2250104123: 'scrd 银色',
+  // 2250104223: 'scrd 红色',
+  // 2150203323: 'speeder d1 亮白',
+  // 2150203223: 'speeder d1 灰色',
+  // 2150203123: 'speeder d1 深邃蓝',
+  2050202123: 'liv alight 2 消光杏紫',
+  2050202223: 'liv alight 2 暗深灰',
 };
 
 // ua
-export const userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36';
+export const userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36';
